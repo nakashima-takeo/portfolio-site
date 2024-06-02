867688a71ab82a67d0cea779a45538b75df29123
nakatake <tonarinototorototoro8972@gmail.com>
2024-06-02 23:08:21 +0900

    change _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import Header from "~/components/header";
+import Footer from "~/components/footer";
+import PortfolioCard from "~/components/portfolio_card";
+import { Portfolio } from "~/types/portfolio";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,11 +11,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const samplePortfolioInfo: Portfolio[] = [
+  {
+    name: "KoT plus",
+    description: "KoT(King of Time)の勤怠情報画面に情報を追加するChrome拡張機能です。",
+    githubUrl: "https://github.com/nakashima-takeo/KoT-plus",
+  },
+  {
+    name: "BestReply",
+    description: "gptを用いて、チケットの返信を自動生成するツールです。",
+    githubUrl: "https://github.com",
+  },
+];
+
 export default function Index() {
   return (
-    <div className=" bg-red-600 pt-10 items-center text-center">
-      <h1 className="underline font-bold">Welcome!</h1>
-      <h1 className="underline font-bold">Welcome!</h1>
+    <div  className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-4 text-center flex-grow">
+        <h1 className="text-3xl font-bold">ポートフォリオ一覧</h1>
+        {samplePortfolioInfo.map((info) => (
+          <PortfolioCard key={info.name} {...info} />
+        ))}
+      </main>
+      <Footer />
     </div>
   );
 }
