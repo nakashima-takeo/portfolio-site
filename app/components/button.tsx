903a56e9f1931de2756ef0c434f96470664d5731
nakatake <tonarinototorototoro8972@gmail.com>
2024-06-03 00:33:06 +0900

    fix button link

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -6,10 +6,10 @@ type ButtonProps = {
 
 export default function Button({text, url}: ButtonProps) {
   return (
-    <button className="px-5 py-4 bg-gray-900 text-gray-100 rounded transition-all duration-250 ease-out hover:translate-y-[-3px] mx-5">
-      <a href={url} target="_blank" rel="noopener noreferrer" className="mx-5">
-        {text}
-      </a>
-    </button>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <button className="px-5 py-4 bg-gray-900 text-gray-100 rounded transition-all duration-250 ease-out hover:translate-y-[-3px] mx-5">
+        <p className="mx-4">{text}</p>
+      </button>
+    </a>
   );
 }
